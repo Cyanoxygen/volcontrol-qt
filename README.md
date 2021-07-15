72725702b20693ce01762c18ec892f0dcc3d1ead
Cyanoxygen <cyanoxygen@aosc.io>
2021-07-15 17:17:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+World's Best Volume Control UI
+------
+
+Inspired by HN article: https://readhacker.news/s/4QqYJ
+
+Requirements
+------
+
+- Python 3.7
+- PyQt5
+- `pulsectl` PyPI package
+
+You can install these by running:
+
+```
+pip3 install -r requirements.txt
+```
+
+Or you would prefer to install them manually:
+
+```
+pip3 install PyQt5 pulsectl
+```
+
+Running
+-----
+
+```
+./main.py
+```
+
+License
+------
+
+WTFPL
---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from typing import List
+import pulsectl
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from ui.mainWindow import Ui_MainWindow
+
+
+def setVolume(pulse: pulsectl.Pulse, sink: pulsectl.PulseSinkInfo, text: str):
+    def setVol():
+        pulse.volume_set_all_chans(sink, float(int(text) / 100.0))
+
+    return setVol
+
+def toggleMute(pulse: pulsectl.Pulse, sink: pulsectl.PulseSinkInfo, src: QCheckBox):
+    def mute():
+        pulse.mute(sink, src.isChecked())
+
+    return mute
+
+def main():
+    sinks = pulsectl.Pulse("TestApp")
+    sinklist = []
+    boxlist: List[QRadioButton] = []
+    for s in sinks.sink_list():
+        sinklist.append(s)
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(window)
+    ui.comboBox.addItems(s.description for s in sinklist)
+    for i in range(1,101):
+        boxlist.append(ui.__dict__[f'radioButton_{i}'])
+    for radiob in boxlist:
+        radiob.clicked.connect(setVolume(sinks, sinklist[ui.comboBox.currentIndex()], radiob.text().split('%')[0]))
+    ui.checkBox.clicked.connect(toggleMute(sinks, sinklist[ui.comboBox.currentIndex()], ui.checkBox))
+    window.show()
+    app.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
